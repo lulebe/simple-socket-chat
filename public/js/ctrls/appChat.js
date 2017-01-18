86dc2295b4138e8bdacc4c55b5bc0f103a29c927
Leander Berg <reg@lulebe.net>
2017-01-18 14:09:14 +0100

    correctly display username in chat

--- public/js/ctrls/appChat.js
+++ public/js/ctrls/appChat.js
@@ -14,14 +14,22 @@ angular.module('chatapp')
       $state.go('app.newChat')
       return
     }
+    chat.messages = chat.messages.map(message => addUsernameToMsg(message, chat.members))
     $scope.chat = chat
     $rootScope.$on('newMessage', function (e, data) {
       if (data.chatid == $stateParams.chatid) {
         $timeout(function () {
-          $scope.chat.messages.push(data.message)
+          $scope.chat.messages.push(addUsernameToMsg(data.message, chat.members))
         })
       }
     })
   })
+
+  function addUsernameToMsg(msg, members) {
+    matches members.filter(member => member._id === msg.by)
+    if (matches.length !== 1)
+      return msg
+    return Object.assign({}, msg, {username: matches[0].name})
+  }
 
 })
