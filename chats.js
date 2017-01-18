c88359d122f8caf16f8d87f0be61b4e351b1354f
Leander Berg <reg@lulebe.net>
2017-01-18 01:15:45 +0100

    almost done implementing mongodb

--- chats.js
+++ chats.js
@@ -1,11 +1,3 @@
-/*
-TODO
-1. use mongo instead of object looksups everywhere
-2. use userIds instead of usernames
-*/
-
-
-
 const UUID = require('node-time-uuid')
 const chatRouter = require('express').Router()
 
@@ -16,61 +8,6 @@ const sockets = require('./sockets')
 const models = require('./db').models
 
 module.exports = {router: chatRouter}
-
-
-//HELPERS
-
-
-//TODO fully implement
-function chatWithoutMessages (chat) {
-  const newChatInstance = Object.assign({}, chat)
-  delete newChatInstance.messages
-  return newChatInstance
-}
-
-//TODO fully implement
-function getOrCreateChat (userA, userB) {
-  const matches = chats.filter(chat =>
-    (chat.userA === userA && chat.userB === userB) ||
-    (chat.userA === userB && chat.userB === userA)
-  )
-  if (matches.length === 1)
-    return matches[0]
-  const chat = {userA, userB, messages: []}
-  chats.push(chat)
-  sockets.sendToUser(
-    userB,
-    eventnames.createdChat,
-    {chat: chat}
-  )
-  return chat
-}
-
-//TODO fully implement
-function insertMessageIntoChat(chat, user, message, cb) {
-  const msg = {
-    by: user,
-    content: message,
-    edited: false
-  }
-  cb()
-}
-
-//TODO fully implement
-function updateMessage(chat, messageId, username, message) {
-  const matches = chat.messages.filter(msg => msg.id === messageId && msg.by === username)
-  if (matches.length !== 1)
-    return null
-  const msg = matches[0]
-  msg.msg = messages
-  msg.edited = true
-  sockets.sendToUser(
-    userB,
-    eventnames.messageUpdate,
-    {chatWith: user, message: msg}
-  )
-  return msg
-}
 
 
 //ROUTES
@@ -102,50 +39,47 @@ chatRouter.post('/', (req, res) => {
     return res.status(400).send({msg: "No chat partners specified"})
   const users = [req.user.id]
   users.concat(req.body.partners)
-  const chat = new models.Chat({
-    members: users,
-    groupName: req.body.groupName,
-    messages: []
-  })
-  chat.save((err, chat) => {
+  models.Chat.createChat(users, req.body.groupName, (err, chat) => {
     if (err)
       return res.status(500).send(err)
     res.status(201).send(chat)
   })
 })
 
-//TODO fully implement
 //post new message into chat if allowed to
 chatRouter.post('/:chatid/message', (req, res) => {
-  if (!req.body.message) {
-    res.status(400).send({msg: "No message was specified"})
-    return
-  }
-  const chat = getChatIfExists(req.user.username, users.findByName(req.params.username).username)
-  if (!chat) {
-    res.status(404).send({msg: "Chat was not found"})
-    return
-  }
-  const message = insertMessageIntoChat(chat, req.user.username, req.body.message)
-  res.status(201).send(message)
+  if (!req.body.message)
+    return res.status(400).send({msg: "No message was specified"})
+  models.Chat.getIfForUser(req.params.chatid, req.user.id, (err, chat) => {
+    if (err)
+      return res.status(500).send(err)
+    if (!chat)
+      return res.status(404).send({msg: "Chat was not found"})
+    chat.addMessageByUser(req.user.id, req.body.message, (err, message) {
+      if (err && err.httpStatus)
+        return res.status(err.httpStatus).send({msg: err.msg})
+      if (err)
+        return res.status(500).send(err)
+      res.status(201).send(message)
+    })
+  })
 })
 
-//TODO fully implement
 //change message if allowed to
 chatRouter.put('/:chatid/message/:messageid', (req, res) => {
-  if (!req.body.message) {
-    res.status(400).send({msg: "No message was specified"})
-    return
-  }
-  const chat = getChatIfExists(req.user.username, users.findByName(req.params.username).username)
-  if (!chat) {
-    res.status(404).send({msg: "Chat was not found"})
-    return
-  }
-  const message = updateMessage(chat, req.params.messageid, req.user.username, req.body.message)
-  if (!message) {
-    res.status(404).send({msg: 'Message was not found'})
-    return
-  }
-  res.status(201).send(message)
+  if (!req.body.message)
+    return res.status(400).send({msg: "No message was specified"})
+  models.Chat.updateMessage(
+    req.params.chatid,
+    req.params.messageid,
+    req.user.id,
+    req.body.message,
+    (err, message) => {
+      if (err && err.httpStatus)
+        return res.status(err.httpStatus).send({msg: err.msg})
+      if (err)
+        return res.status(500).send(err)
+      res.status(201).send(message)
+    }
+  )
 })
--- db.js
+++ db.js
@@ -2,24 +2,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const config = require('./config')
+const socketevents = require('./socketevents.json')
+const sockets = require('./sockets')
 
 const models = {
   User: null,
   Chat: null
 }
-
-//Init mongodb connection
-const init = cb => {
-  mongoose.connect(config.mongoUrl)
-  const db = mongoose.connection
-  db.on('error', console.error.bind(console, 'connection error: '))
-  db.once('open', function() {
-    models.User = mongoose.model('User', userSchema)
-    models.Chat = mongoose.model('Chat', userSchema)
-  });
-}
-
-module.exports = {models, init}
 
 //define Schemas
 
@@ -27,9 +16,11 @@ const userSchema = Schema({
   name: {type: String, required: true},
   password: {type: String, required: true}
 })
+
 userSchema.methods.getChats = function (cb) {
   return this.model('Chat').find({members: this.id}).select('-messages').exec(cb);
 }
+
 
 const chatSchema = Schema({
   groupName: String,
@@ -52,9 +43,73 @@ const chatSchema = Schema({
     edited: Boolean
   }]
 })
+
+chatSchema.statics.createChat = function (members, groupName, cb) {
+  const chat = new this.model('Chat')({
+    members: members,
+    groupName: groupName,
+    messages: []
+  })
+  chat.save((err, chat) => {
+    if (err)
+      return cb(err)
+    chat.members.forEach(member => {
+      sockets.sendToUser(member, socketevents.createdChat, chat)
+    })
+    cb(null, chat)
+  })
+}
+
 chatSchema.statics.getIfForUser = function (chatId, userId, cb) {
   return this.findById(chatId).where({members: userId}).exec(cb)
 }
+
+//TODO fully implement
+chatSchema.statics.updateMessage = function (chatId, messageId, userId, message, cb) {
+  //1. find message
+  //2. check permission (message.by === userId)
+  //3. edit & save message
+  //4. send socket updates
+  //5. call cb
+}
+
 chatSchema.methods.isGroup = function () {
   return this.members.length > 2
 }
+
+chatSchema.methods.addMessageByUser = function (userId, message, cb) {
+  if (this.members.indexOf(userId) >= 0) {
+    this.messages.push({
+      content: message,
+      by: userId,
+      edited: false
+    })
+    this.save((err, chat) => {
+      if (err)
+        return cb(err)
+      const message = chat.messages[chat.messages.length - 1]
+      //socket updates
+      this.members.forEach(member => {
+        sockets.sendToUser(member, socketevents.newMessage, message)
+      })
+      cb(null, message)
+    })
+  } else {
+    cb({httpStatus: 403, msg: "user is not a chat member"})
+  }
+}
+
+
+
+//Init mongodb connection
+const init = cb => {
+  mongoose.connect(config.mongoUrl)
+  const db = mongoose.connection
+  db.on('error', console.error.bind(console, 'connection error: '))
+  db.once('open', function() {
+    models.User = mongoose.model('User', userSchema)
+    models.Chat = mongoose.model('Chat', userSchema)
+  });
+}
+
+module.exports = {models, init}
